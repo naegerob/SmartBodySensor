92df05b3ed6faae750bc3b37402f23263f210b8a
naeger <naeger.robert@gmail.com>
2023-01-09 22:33:17 +0100

    Änderungen zurückgeändert.
    Constraints eher verstanden -> wrap content-> Fragment so gross wie Inhalt

--- EmptyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
+++ EmptyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,10 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.example.myapplication.databinding.ActivityMainBinding
-import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
@@ -21,17 +18,11 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
         setSupportActionBar(binding.toolbar)
-        val navView: BottomNavigationView = binding.navView
+
         val navController = findNavController(R.id.nav_host_fragment_content_main)
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(
-                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
-            )
-        )
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
+
+
 
 
         binding.fab.setOnClickListener { view ->
