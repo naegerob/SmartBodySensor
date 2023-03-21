a8f8fe3f8d7fc1126705991ab761808ff51f3987
naeger <naeger.robert@gmail.com>
2023-03-21 22:31:47 +0100

    optimization, but not tested

--- MasterThesis/app/src/main/java/com/example/myapplication/BluetoothHandler.kt
+++ MasterThesis/app/src/main/java/com/example/myapplication/BluetoothHandler.kt
@@ -12,116 +12,82 @@ import android.util.Log
 import com.example.myapplication.Constants.KEY_DEVICE_ADDRESS
 import java.util.UUID
 
+private const val SCAN_DURATION_MS = 5000L
+
+@SuppressLint("MissingPermission")
 class BluetoothConnectionHandler(private val context: Context) : BluetoothGattCallback() {
 
-
-	private var btPeripheralList = ArrayList<BluetoothDevices>()
-
+	private val peripheralList = listOf(
+		BluetoothDevices("Forerunner735XT", "F8:B6:6B:8E:EF:1F"),
+		BluetoothDevices("Samsung Series TV", "70:2A:D5:52:FF:81"),
+		BluetoothDevices("Petkit", "A4:C1:38:4B:DF:7C"),
+		BluetoothDevices("LEBose", "2C:41:A1:DA:C2:AF"),
+		BluetoothDevices("Smart Body Sensor", "CD:7A:3C:A7:19:3B")
+	)
 
 	companion object {
-		const val TAG = "BluetoothHandler"
-		val UUID_HEART_RATE_SERVICE: UUID				= UUID(0x180D.toLong(), 0x0000.toLong())
-		val UUID_HEART_RATE_CHARACTERISTICS: UUID 		= UUID(0x2A37.toLong(), 0x0000.toLong())
-		val UUID_HEART_RATE_DESCRIPTOR: UUID 			= UUID(0x2902.toLong(), 0x0000.toLong())
+		private const val TAG = "BluetoothHandler"
+		val UUID_HEART_RATE_SERVICE: UUID = UUID.fromString("0000180D-0000-1000-8000-00805F9B34FB")
+		val UUID_HEART_RATE_CHARACTERISTICS: UUID = UUID.fromString("00002A37-0000-1000-8000-00805F9B34FB")
+		val UUID_HEART_RATE_DESCRIPTOR: UUID = UUID.fromString("00002902-0000-1000-8000-00805F9B34FB")
 	}
 
-	init
-	{
-		addPeripheralsInList()
+	init {
+		Log.d(TAG, "Creating BluetoothConnectionHandler")
 	}
-	@SuppressLint("MissingPermission")
-	fun connectBluetoothDevice(device: BluetoothDevice)
-	{
-		val isTargetBlePeripheral = btPeripheralList.any { bleDevice ->
-			bleDevice.macAddress.contains(device.address.toString())
+
+	fun connectBluetoothDevice(device: BluetoothDevice) {
+		val isTargetBlePeripheral = peripheralList.any { bleDevice ->
+			bleDevice.macAddress == device.address.toString()
 		}
-		// Connect only if in Peripheral List
-		if (isTargetBlePeripheral)
-		{
-			Log.d(TAG, device.address.toString())
+		if (isTargetBlePeripheral) {
+			Log.d(TAG, "Connecting to ${device.address}")
 			device.connectGatt(context, false, this)
-		}
-		else
-		{
-			// TODO: Notify not in correct list
+		} else {
+			Log.w(TAG, "${device.address} not in peripheral list")
 		}
 	}
 
-	/**
-	 * Callbacks
-	 */
-	@SuppressLint("MissingPermission")
-	override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int)
-	{
+
+	override fun onConnectionStateChange(gatt: BluetoothGatt, status: Int, newState: Int) {
 		super.onConnectionStateChange(gatt, status, newState)
-
-		if (newState == BluetoothProfile.STATE_CONNECTED)
-		{
+		if (newState == BluetoothProfile.STATE_CONNECTED) {
+			Log.d(TAG, "${gatt.device.address} connected")
 			startNewDataPresenter(gatt.device.address)
-
 			gatt.discoverServices()
-			Log.d(TAG, "Connection successful")
+		} else if (newState == BluetoothProfile.STATE_DISCONNECTED) {
+			Log.d(TAG, "${gatt.device.address} disconnected")
+			gatt.close()
 		}
 	}
 
-	private fun addPeripheralsInList()
-	{
-		// Extend with
-		var btDevice = BluetoothDevices("Forerunner735XT", "F8:B6:6B:8E:EF:1F")
-		btPeripheralList.add(btDevice)
-		btDevice = BluetoothDevices("Samsung Series TV", "70:2A:D5:52:FF:81")
-		btPeripheralList.add(btDevice)
-		btDevice = BluetoothDevices("Petkit", "A4:C1:38:4B:DF:7C")
-		btPeripheralList.add(btDevice)
-		btDevice = BluetoothDevices("LEBose", "2C:41:A1:DA:C2:AF")
-		btPeripheralList.add(btDevice)
-		btDevice = BluetoothDevices("Smart Body Sensor", "CD:7A:3C:A7:19:3B")
-		btPeripheralList.add(btDevice)
-	}
-
-	@SuppressLint("MissingPermission")
 	override fun onServicesDiscovered(gatt: BluetoothGatt, status: Int) {
 		super.onServicesDiscovered(gatt, status)
 		if (status == BluetoothGatt.GATT_SUCCESS) {
-			Log.d(TAG, "Services discovered")
-			// Enable notification
+			Log.d(TAG, "${gatt.device.address} services discovered")
 			val service = gatt.getService(UUID_HEART_RATE_SERVICE)
-			val characteristic = service.getCharacteristic(UUID_HEART_RATE_CHARACTERISTICS)
-
-			characteristic.setWriteType(BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT)
-			val descriptor = characteristic.getDescriptor(UUID_HEART_RATE_DESCRIPTOR)
-			descriptor.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
-			gatt.writeDescriptor(descriptor)
-
-
-		}
-	}
-	override fun onCharacteristicRead(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic, status: Int) {
-		super.onCharacteristicRead(gatt, characteristic, status)
-		if (status == BluetoothGatt.GATT_SUCCESS) {
-			// Characteristic read, do something
-			// TODO: Handle characteristic read
-
-			Log.d(TAG, "Characteristics read")
+			val characteristic = service?.getCharacteristic(UUID_HEART_RATE_CHARACTERISTICS)
+			characteristic?.let { char ->
+				char.writeType = BluetoothGattCharacteristic.WRITE_TYPE_DEFAULT
+				val descriptor = char.getDescriptor(UUID_HEART_RATE_DESCRIPTOR)
+				descriptor?.let { desc ->
+					desc.value = BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE
+					gatt.writeDescriptor(desc)
+				}
+				gatt.setCharacteristicNotification(char, true)
+			}
 		} else {
-			// TODO: Handle failed characteristic read
+			Log.e(TAG, "${gatt.device.address} service discovery failed")
+			gatt.disconnect()
 		}
-	}
-	override fun onCharacteristicChanged(gatt: BluetoothGatt, characteristic: BluetoothGattCharacteristic) {
-		super.onCharacteristicChanged(gatt, characteristic)
-
-		if (characteristic.uuid == UUID_HEART_RATE_CHARACTERISTICS) {
-			val data = characteristic.value
-		// TODO: Process the received data
-		}
-		Log.d(TAG, "Characteristics changed")
 	}
 
 	private fun startNewDataPresenter(targetDeviceAddress: String)
 	{
-		val intent = Intent(context, DataPresenter::class.java)
-		intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-		intent.putExtra(KEY_DEVICE_ADDRESS, targetDeviceAddress)
+		val intent = Intent(context, DataPresenter::class.java).run {
+			flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+			putExtra(KEY_DEVICE_ADDRESS, targetDeviceAddress)
+		}
 		//intent.putExtra(KEY_DEVICE_NAME, btPeripheralList)
 		context.startActivity(intent)
 	}
@@ -131,7 +97,9 @@ class BluetoothScanHandler(private var deviceList: MutableList<BluetoothDevice>,
 						   private var adapter: DeviceAdapter) : ScanCallback() {
 
 	// Bluetooth Handlers
-	private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+	private val bluetoothAdapter: BluetoothAdapter? by lazy(LazyThreadSafetyMode.NONE) {
+		BluetoothAdapter.getDefaultAdapter()
+	}
 	private val bluetoothLeScanner = bluetoothAdapter?.bluetoothLeScanner
 
 	private val bondedDevices = bluetoothAdapter?.bondedDevices
@@ -159,11 +127,9 @@ class BluetoothScanHandler(private var deviceList: MutableList<BluetoothDevice>,
 
 	fun checkBLE() : Boolean
 	{
-		if (bluetoothAdapter == null)
-		{
-			return false
-		}
-		return true
+		return bluetoothAdapter?.let {
+			true
+		} ?: false
 	}
 
 	/**
@@ -173,13 +139,11 @@ class BluetoothScanHandler(private var deviceList: MutableList<BluetoothDevice>,
 	override fun onScanResult(callbackType: Int, result: ScanResult) {
 		super.onScanResult(callbackType, result)
 
-		if(!deviceList.contains(result.device))
-		{
+		if (result.device !in deviceList) {
 			deviceList.add(result.device)
+			adapter.notifyDataSetChanged()
+			Log.d(TAG, "onScanResult called")
 		}
-		// TODO try to realize more specific
-		adapter.notifyDataSetChanged()
-		Log.d(TAG, "onScanResult called")
 	}
 	override fun onScanFailed(errorCode: Int) {
 		super.onScanFailed(errorCode)
